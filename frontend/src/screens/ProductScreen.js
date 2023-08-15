2d6213d4f8fec2565bc21dc918426eb3c9a5b49e
Dennis R <lesdeane99@gmail.com>
2023-08-15 11:13:08 +0800

    Fetched a single product and its descriptions details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,81 +1,78 @@
-import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating2 from '../components/Rating2'
-import axios from 'axios'
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
-
-  const [product, setProduct] = useState([])
-
-  const { id: productId} = useParams()
+  const { id: productId } = useParams()
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId)
   
-  useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${productId}`)
-			setProduct(data)
-		}
-
-		fetchProduct()
-
-	}, [productId])
-
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-      <Row>
-
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-
-        <Col md={4}>
-          <ListGroup variant='flush'>
-
-              <ListGroupItem>
-                <h3>{product.name}</h3>
-              </ListGroupItem>
-
-              <ListGroupItem>
-                <Rating2 value={product.rating} item={product.name} text={`${product.numReviews} reviews`} />
-              </ListGroupItem>
-
-              <ListGroupItem>
-                Price: {product.price}
-              </ListGroupItem>
-
-              <ListGroupItem>
-                Description: {product.description}
-              </ListGroupItem>
-            </ListGroup>
-        </Col>
-        
-        <Col md={3}>
-          <Card>
-          <ListGroup variant='flush'>
-            <ListGroupItem>
-              <Row>
-                <Col>Price:</Col>
-                <Col><strong>${product.price}</strong></Col>
-
-              </Row>
-            </ListGroupItem>
-
-            <ListGroupItem>
-              <Row>
-                <Col>Status:</Col>
-                <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
-              </Row>
-            </ListGroupItem>
-
-            <ListGroupItem>
-              <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
-            </ListGroupItem>
-          </ListGroup>
-          </Card>
-        </Col>
-
-      </Row>
+      { isLoading ? (
+        <h2>Loading..</h2>
+      ) : error ? (
+        <div>
+          { error?.data?.message || error.error}
+        </div>
+      ) : (
+        <>
+          <Row>
+    
+            <Col md={5}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
+    
+            <Col md={4}>
+              <ListGroup variant='flush'>
+    
+                  <ListGroupItem>
+                    <h3>{product.name}</h3>
+                  </ListGroupItem>
+    
+                  <ListGroupItem>
+                    <Rating2 value={product.rating} item={product.name} text={`${product.numReviews} reviews`} />
+                  </ListGroupItem>
+    
+                  <ListGroupItem>
+                    Price: {product.price}
+                  </ListGroupItem>
+    
+                  <ListGroupItem>
+                    Description: {product.description}
+                  </ListGroupItem>
+                </ListGroup>
+            </Col>
+            
+            <Col md={3}>
+              <Card>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col><strong>${product.price}</strong></Col>
+    
+                  </Row>
+                </ListGroupItem>
+    
+                <ListGroupItem>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
+                  </Row>
+                </ListGroupItem>
+    
+                <ListGroupItem>
+                  <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
+                </ListGroupItem>
+              </ListGroup>
+              </Card>
+            </Col>
+    
+          </Row>
+        </>
+      )}
     </>
   )
 }
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -3,13 +3,22 @@ import { apiSlice } from "./apiSLice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    
     getProducts: builder.query({
       query: () => ({
         url: PRODUCT_URL,
       }),
       keepUnusedDataFor: 5,
+    }),
+
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCT_URL}/${productId}`
+      }),
+      keepUnusedDataFor: 5,
     })
+
   }),
 })
 
-export const { useGetProductsQuery } = productsApiSlice
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice
